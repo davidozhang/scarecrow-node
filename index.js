df6e53b5dbf20aa7f4b3a8bde0cb8ed7da99f9f0
L4chong <l4chong@gmail.com>
2015-11-21 17:42:47 -0500

    led and stuff

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 var restify = require("restify");
+var groveSensor = require('jsupm_grove');
 var server = restify.createServer();
 
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
+//-------- Input and Output
+var button = new groveSensor.GroveButton(4);
+var ledRed = new groveSensor.GroveLed(8);
+var ledGreen = new groveSensor.GroveLed(3);
+var ledBlue = new groveSensor.GroveLed(2);
 
+
+
+//------- functions
 function respond(req, res, next)  {
-    console.log("respond works")
-    res.send({hello: "itworks"});
+    console.log("respond is working");
+    var test = button.value();
+    ledRed.on();
+    res.send({button_value: test});
 }
 
 //------- routes
-server.get('/send', respond);
+server.get('/test', respond);
 
 var port = process.env.PORT || 8080;
 server.listen(port, function() {
