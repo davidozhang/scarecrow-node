5f764d40ccb014bd49b69a75761d5903803b48be
L4chong <l4chong@gmail.com>
2015-11-22 13:59:46 -0500

    final

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var groveSensor = require('jsupm_grove');
 var upmBuzzer = require("jsupm_buzzer");
 var server = restify.createServer();
 var request = require('request');
+var upmMicrophone = require("jsupm_mic");
+
+
 
 server.use(restify.queryParser());
 //server.use(restify.bodyParser());
@@ -14,14 +17,45 @@ var ledRed = new groveSensor.GroveLed(8); //device 1
 var ledGreen = new groveSensor.GroveLed(3); //device 2
 var ledBlue = new groveSensor.GroveLed(2); //device 3
 var myBuzzer = new upmBuzzer.Buzzer(6);
+var myMic = new upmMicrophone.Microphone(0);
 
 
 //----- global vars
 var isUserAuthenticated = false;
 var pythonServer = "http://192.168.65.220:5000";
-//var isRedON = false;
-//var isGreenON = false;
-//var isBlueON = false;
+var isRedON = false;
+var isGreenON = false;
+var isBlueON = false;
+
+//---- Mic setup
+var threshContext = new upmMicrophone.thresholdContext;
+threshContext.averageReading = 0;
+threshContext.runningAverage = 0;
+threshContext.averagedOver = 2;
+
+
+var is_running = false;
+//Infinite loop, ends when script is cancelled
+//Repeatedly, take a sample every 2 microseconds;
+//find the average of 128 samples; and
+//print a running graph of the averages
+
+//function mictest(){
+//    var buffer = new upmMicrophone.uint16Array(128);
+//    var len = myMic.getSampledWindow(2, 128, buffer);
+//    if (len)
+//    {
+//        var thresh = myMic.findThreshold(threshContext, 30, buffer, len);
+////        myMic.printGraph(threshContext);
+//        if (thresh)
+////            console.log("Threshold is " + thresh);
+//        if (thresh > 300) isUserAuthenticated = true;
+//    }
+//};
+//
+//micinterval = setInterval(mictest, 1000);
+
+
 
 //------- functions
 function auth(req, res, next) {
@@ -55,29 +89,35 @@ function devicescontroller(req, res, next)  {
     if(device == "Optional(\"device_1\")"){
         if(devicestatus == "on") {
             console.log("turn on RED");
-            ledRed.on();
+//            ledRed.on();
+            isRedON = true;
         }
         else {
             console.log("turn off RED");
             ledRed.off();
+            isRedON = false;
         }
     } else if (device == "Optional(\"device_2\")"){
         if(devicestatus == "on") {
             console.log("turn on Green");
-            ledGreen.on();
+//            ledGreen.on();
+              isGreenON = true;
         }
         else {
             console.log("turn on Green");
             ledGreen.off();
+              isGreenON = false;
         }
     } else if(device == "Optional(\"device_3\")"){
         if(devicestatus == "on"){
             console.log("turn on Blue")
-            ledBlue.on();
+//            ledBlue.on();
+            isBlueON = true;
         }
         else {
             console.log("turn off Blue")
             ledBlue.off();
+            isBlueON = false;
         }
     }else{
         console.log("error");
@@ -139,6 +179,9 @@ function readButtonValue() {
                 request(pythonServer + '/sendtotwitter' , function (error, response, body) {
                     if (!error && response.statusCode == 200) {
                         console.log('sent to twitter');
+                        isGreenON = true;
+                        isBlueON = true;
+                        isRedON = true;
                     }
                 })
             }
@@ -147,47 +190,59 @@ function readButtonValue() {
 }
 
 
-//var blueflag = false;
-//var redflag = false;
-//var greenflag = false;
+var blueflag = false;
+var redflag = false;
+var greenflag = false;
 
+function lightsController(){
+    if(isGreenON){
+        if(greenflag){
+            ledGreen.on();
+//            console.log('green is on');
+            greenflag = false;
+        }else{
+            ledGreen.off();
+//            console.log('green is off');
+            greenflag = true;
+        }
+    }
 
-//function lightsController(){
-//    if(isGreenON){
-//        if(greenflag){
-//            ledGreen.off();
-//            console.log('greenoff');
-//            greenflag == false;
-//        }else{
-//            ledGreen.on();
-//            console.log('greentrue');
-//            greenflag == true;
-//        }
-//    }
-//    if(isRedON){
-//        if(redflag){
-//            ledRed.off();
-//            redflag == false;
-//        }else{
-//            ledRed.on();
-//            redflag == false;
-//        }
-//    }
-//    if(isBlueON){
-//        if(blueflag){
-//            ledBlue.off();
-//            blueflag == false;
-//        }else{
-//            ledBlue.on();
-//            blueflag == true;
-//        }
-//    }
-//}
+    if(isRedON){
+        if(redflag){
+            ledRed.on();
+//            console.log('red is on');
+            redflag = false;
+        }else{
+            ledRed.off();
+//            console.log('red is off');
+            redflag = true;
+        }
+    }
 
-//setInterval(lightsController, 1000);
+    if(isBlueON){
+        if(blueflag){
+            ledBlue.on();
+//            console.log('blue is on');
+            blueflag = false;
+        }else{
+            ledBlue.off();
+//            console.log('blue is off');
+            blueflag = true;
+        }
+    }
+
+}
+
+setInterval(lightsController, 300);
 
 function reset(req, res, next) {
     isUserAuthenticated = false;
+    ledBlue.off();
+    ledRed.off();
+    ledGreen.off();
+    isBlueON = false;
+    isRedON = false;
+    isGreenON = false;
     res.send('reset');
 }
 
